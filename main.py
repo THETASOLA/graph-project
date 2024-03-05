1205063245f0014f5a07cf43cfe474b4690b8704
Tao Solan <tao.solan@gmail.com>
2024-03-05 11:10:24 +0100

    change data format and cyclic validator

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from showGraph import generateGraph, drawGraph
+from dataValidator import is_cyclic
 
-testG = [{"A": ["B", "C", "D", "E", "F", "G", "H"]}, 
-         {"B": ["C", "D", "E"]}, 
-         {"C": ["F", "G"]},
-         {"D": ["E"]},
-         {"E": ["D", "G"]},
-         {"F": ["C", "H"]},
-         {"G": ["C", "E"]},
-         {"H": ["F"]}]
+testG = {
+    "A": ["B", "C", "D", "E", "F", "G", "H"],
+    "B": ["C", "D", "E"],
+    "C": ["F", "G"],
+    "D": ["E"],
+    "E": ["D", "G"],
+    "F": ["C", "H"],
+    "G": ["C", "E"],
+    "H": ["F"]
+}
 
 generateGraph(testG)
-drawGraph(testG, True)
+print("Is cyclic; ",is_cyclic(testG))
+#drawGraph(testG, True)
